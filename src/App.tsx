9265e24c94a3bce9afc1918cc43b35ef0eb76d13
akanksha-c247 <akanksha.k@chapter247.com>
2023-08-09 18:34:16 +0530

    crud with scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import './App.scss'
+import { Book } from './Employee.type';
 
 function App() {
+  const [books, setBooks] = useState<Book[]>([
+    { id: 1, title: 'Book 1', author: 'Author 1' },
+    { id: 2, title: 'Book 2', author: 'Author 2' },
+  ]);
+
+  const [formData, setFormData] = useState<Book>({ id: 0, title: '', author: '' });
+
+  const handleInputChange = (fieldName: keyof Book, value: string) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [fieldName]: value,
+    }));
+  };
+
+  const addBook = () => {
+    if (!formData.title || !formData.author) return;
+
+    const newBook: Book = { ...formData, id: Date.now() };
+    setBooks([...books, newBook]);
+    setFormData({ id: 0, title: '', author: '' });
+  };
+
+  const updateBook = (id: number) => {
+    const updatedBooks = books.map((book) =>
+      book.id === id ? { ...book, ...formData } : book
+    );
+    setBooks(updatedBooks);
+    setFormData({ id: 0, title: '', author: '' });
+  };
+
+  const deleteBook = (id: number) => {
+    const updatedBooks = books.filter((book) => book.id !== id);
+    setBooks(updatedBooks);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Book CRUD Application</h1>
+      <div className='form-container'>
+        <input
+          type="text"
+          placeholder="Title"
+          value={formData.title}
+          onChange={(e) => handleInputChange('title', e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Author"
+          value={formData.author}
+          onChange={(e) => handleInputChange('author', e.target.value)}
+        />
+        {formData.id === 0 ? (
+          <button onClick={addBook}>Add Book</button>
+        ) : (
+          <button onClick={() => updateBook(formData.id)}>Update Book</button>
+        )}
+      </div>
+      <ul className='book-list'>
+        {books.map((book) => (
+          <li key={book.id}>
+            <span>{book.title}</span> by <span>{book.author}</span>
+            <button onClick={() => setFormData(book)}>Edit</button>
+            <button onClick={() => deleteBook(book.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
